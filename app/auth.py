b013d35c584cb3adc844fcf528d0e710096a93d8
Marcelo Ferreira <marcelonferr10@gmail.com>
2026-07-23 14:41:49 -0300

    feat: testing

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,7 @@ from fastapi import Request
 
 
 class NotAuthenticated(Exception):
-    """Raised by `require_login` when there is no logged-in user."""
+    """Raised by `require_login` when there is no logged-in user. teste"""
 
 
 def get_current_user(request: Request) -> str | None:
